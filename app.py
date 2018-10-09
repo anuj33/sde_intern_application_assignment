4696a69903c9ee436c568e8f6ce3ec3d16a5ffa9
vaibhavgupta.v <vaibhavgupta.v@flipkart.com>
2018-10-09 12:25:50 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import json
+from flask import Flask, request, jsonify
+import os
+from DiagnosisClient import DiagnosisClient
+import config
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+diagnosisClient = DiagnosisClient(config.username, config.password, config.priaid_authservice_url, config.language, config.priaid_healthservice_url)
+
+# i have taken the help of publicly available code on priaid
+# fetch all the symptoms present in medic records
+@app.route("/fetch_all_symptoms", methods=["GET"])
+def fetch_all_symptoms():
+    return jsonify(diagnosisClient.loadSymptoms())
+
+@app.route("/fetch_proposed_symptom", methods=["POST"])
+def fetch_proposed_symptom():
+    payload_data = json.loads(request.data)
+    symptom_name = payload_data.get("symptom")
+    gender = payload_data.get("gender")
+    year = payload_data.get("birth_year")
+    return json.dumps(diagnosisClient.loadProposedSymptoms(symptom_name, gender, year))
+
+@app.route("/fetch_medical_condition_by_symptom_ids", methods=["POST"])
+def fetch_symptoms_by_params():
+    payload_data = json.loads(request.data)
+    symptom_ids = payload_data.get("symptom_ids")
+    gender = payload_data.get("gender")
+    year = payload_data.get("birth_year")
+    return json.dumps(diagnosisClient.loadMedicalIssues(symptom_ids, gender, year))
+
+@app.route("/fetch_treatement_option_by_medical_condition", methods=["POST"])
+def fetch_treatment_option_by_medical_condition():
+    payload_data = json.loads(request.data)
+    medical_condition = payload_data.get("medical_condition")
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,6 @@
+username = "2011a8ps255p@gmail.com"
+password = "j5N3Qiz7MAf6r2K9Y"
+priaid_authservice_url = "https://sandbox-authservice.priaid.ch/login"
+priaid_healthservice_url = "https://sandbox-healthservice.priaid.ch"
+language = "en-gb"
+pritnRawOutput = False
